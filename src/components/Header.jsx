3d4a3cd98cf601ea62f218162b4dd4fbed6ef064
Ajay-Bankar <bankarajay116@gmail.com>
2025-01-16 11:55:59 +0530

    changes push

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className=' text-blue-800 text-2xl font-bold'>this is header</h1>
+    </div>
+  )
+}
+
+export default Header
